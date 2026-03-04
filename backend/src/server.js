397b0f8bac4a599afa3569b70194ce63f4d97a59
bikashdalapati-09 <bikashdalapati09@gmail.com>
2026-03-04 23:06:03 +0530

    Initial backend setup with MongoDB connection

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import app from "./app.js";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected Successfully");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("Problem in mongoDB connection");
+    
+  })
